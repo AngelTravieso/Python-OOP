ee65e5e153698f53ae1b0df3483a8455a6b1635b
AngelTravieso <angeltraviesoc@gmail.com>
2024-04-11 15:42:12 -0400

    Tipos de metodos

--- 01-clases.py
+++ 01-clases.py
@@ -9,11 +9,28 @@ class Pajaro:
         self.color = color
         self.especie = especie
 
+    # Métodos de instancia
     def piar(self):
         print('pio, mi color es {}'.format(self.color))
 
     def volar(self, metros):
         print(f'El pajaro ha volado {metros} metros')
+        self.piar()
+
+    def pintar_negro(self):
+        self.color = 'negro'
+        print(f'Ahora el pajaro es color {self.color}')
+
+    # Método de clase
+    @classmethod
+    def poner_huevos(cls, cantidad):
+        print(f'Puso {cantidad} de huevos')
+        cls.alas = False
+        print(Pajaro.alas)
+
+    @staticmethod
+    def mirar():
+        print('El pajaro mira')
 
 
 mi_pajaro = Pajaro('negro', 'Tucan')
@@ -32,3 +49,12 @@ print(f'Mi pajaro es un {mi_pajaro.especie} y es de color {mi_pajaro.color}')
 
 print(mi_pajaro.piar())
 print(mi_pajaro.volar(50))
+
+# Modificar atributo de clase
+mi_pajaro.alas = False
+print(mi_pajaro.alas)
+
+Pajaro.poner_huevos(3)
+# Pajar.piar() # Da error porque piar() no es un método de clase
+
+Pajaro.mirar()
