7192a9bac6d963c36e9b002e69751de8e19f52b1
AngelTravieso <angeltraviesoc@gmail.com>
2024-04-11 15:19:57 -0400

    Atributos

--- 01-clases.py
+++ 01-clases.py
@@ -1,9 +1,25 @@
 class Pajaro:
-    pass
 
-mi_pajaro = Pajaro()
-otro_pajaro = Pajaro()
+    # Atributo de clase
+    alas = True
+
+    # Init = constructor, self = instancia del objeto que será creado, self es una convención
+    # Atributo de instancia
+    def __init__(self, color, especie):
+        self.color = color
+        self.especie = especie
+
+
+mi_pajaro = Pajaro('negro', 'Tucan')
+otro_pajaro = Pajaro('amarillo', 'Turpial')
 
 print(mi_pajaro)
 
+# Conocer el tipo de la variable
 print(type(mi_pajaro))
+
+print(mi_pajaro.color)
+print(mi_pajaro.especie)
+print(Pajaro.alas)
+
+print(f'Mi pajaro es un {mi_pajaro.especie} y es de color {mi_pajaro.color}')
